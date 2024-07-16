81474ee2142fdf99d91f1c6adf310f4dfd827670
SimoneColombo1 <simcolombo2@gmail.com>
2024-07-16 22:43:28 +0200

    Creata la struttura dell' header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,6 @@
 
 @section('main-content')
     <h1>
-        Homepage
+
     </h1>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,2 +1,24 @@
 <header>
+<div>
+<span>LOGO</span>
+<ul>
+    <li><a href="">CHARACTERS</a></li>
+    <li><a href="">COMICS</a></li>
+    <li><a href="">MOVIES</a></li>
+    <li><a href="">TV</a></li>
+    <li><a href="">GAMES</a></li>
+    <li><a href="">COLLECTIBLES</a></li>
+    <li><a href="">VIDEOS</a></li>
+    <li><a href="">FANS</a></li>
+    <li><a href="">NEWS</a></li>
+    <li><a href="">SHOP</a></li>
+
+</ul>
+
+
+</div>
+
+
+
+
 </header>
